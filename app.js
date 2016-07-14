bd0d490d26a735a1392fa29d57f292da0a4bb781
luff1012 <suyash1212@gmail.com>
2016-07-15 01:17:09 +0530

    Better error message 2

--- app.js
+++ app.js
@@ -134,8 +134,8 @@ function receivedMessage(event) {
   var timeOfMessage = event.timestamp;
   var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:",
-    senderID, recipientID, timeOfMessage);
+  console.log("Received message for user %d and page %d at %d with message: %d",
+    senderID, recipientID, timeOfMessage,message);
   //console.log(JSON.stringify(message));
 
   var messageId = message.mid;
