ee616fff4e6249401508e0e9a5842319f76eb425
luff1012 <suyash1212@gmail.com>
2016-07-14 19:44:19 +0530

    Add postback call

--- app.js
+++ app.js
@@ -60,6 +60,22 @@ function receivedDeliveryConfirmation(event) {
   console.log("All message before %d were delivered.", watermark);
 }
 
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback
+  // button for Structured Messages.
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
+}
 
 app.post('/webhook', function (req, res) {
   var data = req.body;
